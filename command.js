702091b298a6ad0d442387d5b17faa2644237fc0
cherish <xieqin1991@gmail.com>
2019-09-01 19:03:43 +0800

    Update command.js

--- command.js
+++ command.js
@@ -1,3 +1,7 @@
+/**
+* 最简形式的命令对象是一个操作和用以调用这个操作对象的结合体
+* 所有的命令对象都有一个执行操作（execute operation），其用途就是调用命令对象所绑定的操作（action）
+**/
 const AdCommand = new Interface('AdCommand', ['execute'])
 
 const StopAd = function (adObject) {
